9992fcc393249df0c4b3e5bf4db3ed9f961f9b23
Renan T. Carneiro <renantc@uspgamedev.org>
2014-11-19 04:16:54 -0200

    Host

---
+++ host.rb
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+class Host
+
+  def initialize (ip, router)
+    @ip = ip
+    @router = router
+    @applications = Hash.new
+  end
+
+  def sendToHost(package, hostname)
+    ip = getIpByHostname(hostname)
+    self.sendToIp(package, ip)
+  end
+
+  def sendToIp(package, ip)
+    @router.sendToIp(package, ip)
+  end
+
+  def openPort(application, port)
+    if @applications[port] == nil
+      @applications[port] = application
+    else
+      print("derp\n")
+      #TODO: ver exceções
+    end
+  end
+
+  def closePort(port)
+    @applications[port] = nil
+  end
+
+  def receive(package)
+    port = package.getPort
+    @applications[port].getPackage(package)
+  end
+
+end
